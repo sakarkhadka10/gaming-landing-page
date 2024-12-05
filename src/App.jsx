37f9565b957957a77ce445179ac703044278abab
Sakar Khadka <khadka.sakar10@gmail.com>
2024-12-05 22:03:06 +0545

    'inital'

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Hero />
+      </main>
+    </>
+  );
+};
+
+export default App;
